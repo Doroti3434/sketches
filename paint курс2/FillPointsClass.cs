6344e5b745f189d57addf10c7bc5d9758367e260
20erik03 20erik03 <margiev_e@mail.ru>
2022-05-16 02:18:46 +0300

    Добавила комментарии

--- paint курс2/FillPointsClass.cs
+++ paint курс2/FillPointsClass.cs
@@ -8,34 +8,35 @@ namespace paint_курс2
 {
     internal static class FillPointsClass
     {
+        //Проверка пикселя на замену цвета и добавление в стек
         private static void validate(Bitmap bm, Stack<Point> sp, int x, int y, Color old_color, Color new_color)
         {
-            Color cx = bm.GetPixel(x, y);
-            if (cx == old_color)
+            Color cx = bm.GetPixel(x, y);//Получить цвет пикселя по координатам
+            if (cx == old_color) //Если это изменяемый цвет
             {
-                sp.Push(new Point(x, y));
-                bm.SetPixel(x, y, new_color);
+                sp.Push(new Point(x, y));//Закинуть в стек пиксель
+                bm.SetPixel(x, y, new_color);//Покрасить пиквель в новый цвет
             }
         }
 
+        //Заливка
         public static void Fill(Bitmap bm, int x, int y, Color new_clr) // загадка вселенной
         {
-            Color old_color = bm.GetPixel(x, y);
-            Stack<Point> pixel = new Stack<Point>();
-            pixel.Push(new Point(x, y));
-            bm.SetPixel(x, y, new_clr);
-            if (old_color == new_clr) return;
+            Color old_color = bm.GetPixel(x, y); //Получить старый цвет
+            Stack<Point> pixel = new Stack<Point>(); //Стек пикселями, которые надо поменять
+            pixel.Push(new Point(x, y)); //Закинуть пиксель на который нажали
+            bm.SetPixel(x, y, new_clr); //Обновить цвет в пикселе на новый
+            if (old_color == new_clr) return; //Если цвет один и тот же завершить
 
-            while (pixel.Count > 0)
+            while (pixel.Count > 0) //Пока стек не пустой
             {
-                Point pt = (Point)pixel.Pop();
-                if (pt.X > 0 && pt.Y > 0 && pt.X < bm.Width - 1 && pt.Y < bm.Height - 1)
+                Point pt = (Point)pixel.Pop(); //Получить координаты пикселя
+                if (pt.X > 0 && pt.Y > 0 && pt.X < bm.Width - 1 && pt.Y < bm.Height - 1) //Проверка на выход за рамки
                 {
-                    validate(bm, pixel, pt.X - 1, pt.Y, old_color, new_clr);
-                    validate(bm, pixel, pt.X, pt.Y - 1, old_color, new_clr);
-                    validate(bm, pixel, pt.X + 1, pt.Y, old_color, new_clr);
-                    validate(bm, pixel, pt.X, pt.Y + 1, old_color, new_clr);
-
+                    validate(bm, pixel, pt.X - 1, pt.Y, old_color, new_clr); //Проверить левую клетку
+                    validate(bm, pixel, pt.X, pt.Y - 1, old_color, new_clr); //Проверить верхнюю клетку
+                    validate(bm, pixel, pt.X + 1, pt.Y, old_color, new_clr); //Проверить правую клетку
+                    validate(bm, pixel, pt.X, pt.Y + 1, old_color, new_clr); //Проверить нижнюю клетку
                 }
             }
 
--- paint курс2/Form1.cs
+++ paint курс2/Form1.cs
@@ -6,53 +6,46 @@ namespace paint_курс2
         {
             InitializeComponent();
 
-            this.Width = 900;
-            this.Height = 700;
-            bm = new Bitmap(pic.Width, pic.Height);
-            g = Graphics.FromImage(bm);
-            g.Clear(Color.White);
-            pic.Image = bm;
-
+            this.Width = 900; //Ширина  
+            this.Height = 700; //Высота
+            bm = new Bitmap(pic.Width, pic.Height); //Создание поля с задаными высотой и шириной
+            g = Graphics.FromImage(bm); //Создание объекта класса Graphics для Bitmap
+            g.Clear(Color.White); //Заполнение поля белым цветом
+            pic.Image = bm; //Прорисовка Bitmap на PictureBox
         }
 
-
-
-        Bitmap bm;
-        Graphics g;
-        bool paint = false;
-        Point px, py;
-        Pen p = new Pen(Color.Black, 5);
-        Pen erase = new Pen(Color.White, 10);
+        Bitmap bm; //Поле на котором мы рисуем (холст)
+        Graphics g; //Инструмент с помощью которого мы рисуем
+        bool paint = false; //Флажок указывающие на нажатие мыши
+        Point px, py; //Координаты (х;у)
+        Pen p = new Pen(Color.Black, 5); //Карандаш знач. по ум. 
+        Pen erase = new Pen(Color.White, 10); //Ластик
         Color new_color;
-        ColorDialog cd = new ColorDialog();
-       
+        ColorDialog cd = new ColorDialog(); //Палитра цветов 
+
         int index;
-        int x, y, sX, sY, cX, cY;
+        int x, y, sX, sY, cX, cY; //Координаты (х;у)
 
 
-        //движения мыши
+        //Событие движения мыши
         private void pic_MouseMove(object sender, MouseEventArgs e)
         {
-            if (paint)
+            if (paint) //Если мышь зажата
             {
-                if (index == 2)
+                if (index == 2) //Выбран режим ластика
                 {
                     px = e.Location;
-                    g.DrawLine(erase, px, py);
+                    g.DrawLine(erase, px, py); //Нарисовать линию с помощью erase (Ластика)
                     py = px;
                 }
-
-
-                if (index == 1)
+                if (index == 1) //Выбран режим карандаша 
                 {
                     px = e.Location;
-                    g.DrawLine(p, px, py);
+                    g.DrawLine(p, px, py); //Нарисовать линию с помощью p (Карандаша)
                     py = px;
                 }
-
-
             }
-            pic.Refresh();
+            pic.Refresh(); // Обновить PictureBox
 
             x = e.X;
             y = e.Y;
@@ -60,136 +53,133 @@ namespace paint_курс2
             sY = e.Y - cY;
         }
 
+        //Отжатие мыши
         private void pic_MouseUp(object sender, MouseEventArgs e)
         {
-            paint = false;
+            paint = false; //изменение флажка, мышь отжата
 
             sX = x - cX;
             sY = y - cY;
 
-            if (index == 3)
+            if (index == 3) //Выбран режим рисования элипса
             {
-                g.DrawEllipse(p, cX, cY, sX, sY);
+                g.DrawEllipse(p, cX, cY, sX, sY); //Нарисовать элипс
             }
-
-            if (index == 4)
+            if (index == 4) //Выбран режим рисования прямоугольника
             {
-                g.DrawRectangle(p, cX, cY, sX, sY);
+                g.DrawRectangle(p, cX, cY, sX, sY); //Нарисовать прямоугольник
             }
-            if (index == 5)
+            if (index == 5) //Выбран режим рисования прямой линии
             {
-                g.DrawLine(p, cX, cY, x, y);
+                g.DrawLine(p, cX, cY, x, y); //Нарисовать линию
             }
-
         }
+
+        //Заливка
         private void pic_MouseClick(object sender, MouseEventArgs e)
         {
-            if (index == 7)
+            if (index == 7) //Выбран режим заливки
             {
-                Point point = set_point(pic, e.Location);
-                FillPointsClass.Fill(bm, point.X, point.Y, new_color);
+                Point point = set_point(pic, e.Location); //Координаты клика
+                FillPointsClass.Fill(bm, point.X, point.Y, new_color); //Заливка
             }
         }
+
+        //Зажатие мыши
         private void pic_MouseDown(object sender, MouseEventArgs e)
         {
-            paint = true;
+            paint = true; //изменение флажка, мышь зажата
             py = e.Location;
 
             cX = e.X;
             cY = e.Y;
-
-
         }
-
-
-
-
 
         //фигуры и линии
         private void btn_pencil_Click(object sender, EventArgs e)
         {
-            index = 1;
-            p.StartCap = System.Drawing.Drawing2D.LineCap.Round;
-            p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            index = 1; //Установка индекса на режим карандаш
+            p.StartCap = System.Drawing.Drawing2D.LineCap.Round;//Выравнивание линии
+            p.EndCap = System.Drawing.Drawing2D.LineCap.Round;//Выравнивание линии
         }
         private void btn_eraser_Click(object sender, EventArgs e)
         {
-            index = 2;
+            index = 2; //Установка индекса на режим ластик
         }
         private void btn_ellipse_Click(object sender, EventArgs e)
         {
-            index = 3;
+            index = 3; //Установка индекса на режим элипс
         }
         private void btn_rect_Click(object sender, EventArgs e)
         {
-            index = 4;
+            index = 4; //Установка индекса на режим прямоугольник
         }
         private void btn_line_Click(object sender, EventArgs e)
         {
-            index = 5;
+            index = 5; //Установка индекса на режим линия
         }
         private void btn_fill_Click(object sender, EventArgs e)
         {
-            index = 7;
+            index = 7; //Установка индекса на режим заливка
         }
 
 
 
-        // палетка сохраняшка и клинер
+        //Всплывающее окно с выбором цвета
         private void btn_color_Click(object sender, EventArgs e)
         {
-            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            if (colorDialog1.ShowDialog() == DialogResult.OK) // Если нажали ОК
             {
-                p.Color = colorDialog1.Color;
-                pic_color.BackColor = colorDialog1.Color;
-                new_color = cd.Color;
-               // p.Color = pic_color.BackColor;
-
+                p.Color = colorDialog1.Color; //Изменить цвет Pen p
+                pic_color.BackColor = colorDialog1.Color; //Изменить цвет кнопки pic_color
+                new_color = cd.Color; //Запись выбранного цвета           
             }
         }
 
+        //Очистка поля
         private void btn_Clear_Click(object sender, EventArgs e)
         {
-            g.Clear(pic.BackColor);
-            pic.Image = bm;
+            g.Clear(pic.BackColor); // Очистить Bitmap
+            pic.Image = bm; // Обновить PictureBox
         }
 
+        //Сохраненить изображение
         private void btn_saver_Click(object sender, EventArgs e)
         {
-            saveFileDialog1.Filter = "JPG(*.JPG)|*.jpg";
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            saveFileDialog1.Filter = "JPG(*.JPG)|*.jpg"; // Задаём формат
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) // Если нажали ОК
             {
-                if (pic.Image != null)
+                if (pic.Image != null) // Если картика не пустая
                 {
-                    pic.Image.Save(saveFileDialog1.FileName);
+                    pic.Image.Save(saveFileDialog1.FileName); // Сохранить картинку в заданом пути
                 }
             }
         }
 
-      
 
+        //Рисование
         private void pic_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
 
-            if (paint)
+            if (paint) // Если мышь зажата
             {
-                if (index == 3)
+                if (index == 3) //Выбран режим рисования элипса
                 {
-                    g.DrawEllipse(p, cX, cY, sX, sY);
+                    g.DrawEllipse(p, cX, cY, sX, sY); //Нарисовать элипс
                 }
-
-                if (index == 4)
+                if (index == 4) //Выбран режим рисования прямоугольника
                 {
-                    g.DrawRectangle(p, cX, cY, sX, sY);
+                    g.DrawRectangle(p, cX, cY, sX, sY); //Нарисовать прямоугольник
                 }
-                if (index == 5)
+                if (index == 5) //Выбран режим рисования прямой линии
                 {
-                    g.DrawLine(p, cX, cY, x, y);
+                    g.DrawLine(p, cX, cY, x, y); //Нарисовать линию
                 }
             }
         }
 
+        //Перевод координат в Point относительно PictureBox
         static Point set_point(PictureBox pb, Point pt)
         {
             float pX = 1f * pb.Image.Width / pb.Width;
@@ -197,15 +187,14 @@ namespace paint_курс2
             return new Point((int)(pt.X * pX), (int)(pt.Y * pY));
         }
 
+        //Пипетка
         private void color_picker_MouseClick(object sender, MouseEventArgs e)
         {
             Point point = set_point(color_picker, e.Location);
-            pic_color.BackColor = ((Bitmap)color_picker.Image).GetPixel(point.X, point.Y);
-            new_color = pic_color.BackColor;
-            
-            p.Color = pic_color.BackColor;
-            
-        }
+            pic_color.BackColor = ((Bitmap)color_picker.Image).GetPixel(point.X, point.Y);// Получить цвет фона
+            new_color = pic_color.BackColor; 
 
+            p.Color = pic_color.BackColor; // Обновить цвет Pen p
+        }
     }
 }
